38d91c06ba7ca33aa144eafc4c5a183fefaf04ef
LeoZion <leozionsuporte@gmail.com>
2022-09-17 22:28:21 -0300

    add event open modal dailog

--- src/components/AdBanner.tsx
+++ src/components/AdBanner.tsx
@@ -1,3 +1,5 @@
+import * as Dialog from '@radix-ui/react-dialog';
+
 import { MagnifyingGlassPlus } from "phosphor-react";
 
 export function AdBanner() {
@@ -8,10 +10,10 @@ export function AdBanner() {
           <h3 className="text-2xl text-white font-black">Não encontrou o seu duo?</h3>
           <span className="text-zinc-300">Publique um anúncio para encontrar novos players</span>
         </div>
-        <button className="flex items-center gap-3 bg-violet-500 hover:bg-violet-600 text-white rounded px-4 py-3">
+        <Dialog.Trigger className="flex items-center gap-3 bg-violet-500 hover:bg-violet-600 text-white rounded px-4 py-3">
           <MagnifyingGlassPlus size={24} />
           Publicar anúncio
-        </button>
+        </Dialog.Trigger>
       </div>
     </div>
   )
