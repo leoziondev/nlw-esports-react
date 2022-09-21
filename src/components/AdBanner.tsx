88ecedae1236fc0d90281df1faf81f8ad072a879
LeoZion <leozionsuporte@gmail.com>
2022-09-21 18:47:18 -0300

    responsive banner

--- src/components/AdBanner.tsx
+++ src/components/AdBanner.tsx
@@ -5,8 +5,8 @@ import { MagnifyingGlassPlus } from "phosphor-react";
 export function AdBanner() {
   return (
     <div className="pt-1 bg-nlw-gradient self-stretch rounded-lg overflow-hidden mt-8">
-      <div className="bg-[#2a2634] px-8 py-6 flex justify-between items-center">
-        <div>
+      <div className="bg-[#2a2634] px-8 py-6 flex flex-col md:flex-row justify-between items-center">
+        <div className="mb-6 md:mb-0">
           <h3 className="text-2xl text-white font-black">Não encontrou o seu duo?</h3>
           <span className="text-zinc-300">Publique um anúncio para encontrar novos players</span>
         </div>
